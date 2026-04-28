f1c7b50e9c10a83e07d44af63e534bc11f68c971
MdShajalalsojib <shahjalalsojib088@gmail.com>
2026-04-28 23:58:09 +0600

    fdi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Coding from './pages/Coding';
@@ -14,21 +14,23 @@ import Skills from './pages/Skills';
 const App = () => {
   return (
       
-    <BrowserRouter> 
-         <Routes> 
-         <Route path='/'element={Home}></Route>
-         <Route path='/about'element={About}></Route>
-         <Route path='/coding'element={Coding}></Route>
-         <Route path='/contact'element={Contact}></Route>
-         <Route path='/education'element={Education}></Route>
-         <Route path='/experience'element={Experience}></Route>
-         <Route path='/projects'element={Projects}></Route>
-         <Route path='/skills'element={Skills}></Route>
-         </Routes>
-       
-     </BrowserRouter> 
      
-  )
+          <>
+          <Routes> 
+         <Route path='/' element={Home }/>
+         <Route path='/about'element={About }/>
+         <Route path='/coding'element={Coding }/>
+         <Route path='/contact'element={Contact }/>
+         <Route path='/education'element={Education }/>
+         <Route path='/experience'element={Experience }/>
+         <Route path='/projects'element={Projects }/>
+         <Route path='/skills'element={Skills }/>
+         </Routes>
+          </>
+       
+      
+     
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
- import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -8,3 +8,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 );
+
+ 
